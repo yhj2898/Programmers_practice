3c6e85c5fed97243f1cdef98f6893e2a4b40e1d4
yhj2898 <yhj2898@naver.com>
2025-11-10 17:22:00 +0900

    [level 0] Title: 홀수 vs 짝수, Time: 0.01 ms, Memory: 9.27 MB -BaekjoonHub

---
+++ 프로그래머스/0/181887. 홀수 vs 짝수/홀수 vs 짝수.py
@@ -0,0 +1,10 @@
+def solution(num_list):
+    answer = 0
+    odd = 0
+    even = 0
+    for i in range(len(num_list)):
+        if i%2 ==0:
+            even += num_list[i]
+        else:
+            odd += num_list[i]
+    return max(odd, even)
